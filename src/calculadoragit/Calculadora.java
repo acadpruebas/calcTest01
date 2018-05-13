566db726bb6edef7a2eb81478b2d0b9e07ed8b14
Un usuario <pruebas@usuario.local>
2018-05-13 18:31:15 +0100

    multiplica

--- src/calculadoragit/Calculadora.java
+++ src/calculadoragit/Calculadora.java
@@ -13,5 +13,8 @@ public class Calculadora {
     public static int suma (int a, int b){
         return a+b;
     }
+    public static int multiplicacion(int a, int b){
+        return a*b;
+    }
 }
 
